37507333683787f6056f4959e294db471a09c577
Rayhan Abdullah <programmer.rayhan1@gmail.com>
2025-03-17 12:18:18 +0600

    added domain

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,3 +1,3 @@
 export const SITE_DOMAIN = process.env.NEXT_PUBLIC_SITE_DOMAIN || 'http://localhost:3000/';
-export const BACKEND_BASE_URL = process.env.EXPRESS_BASE_URL || 'http://localhost:4000';
-export const EXPRESS_VERSION_URL = process.env.EXPRESS_VERSION_URL || 'http://localhost:4000/api/v1';
+export const BACKEND_BASE_URL = process.env.EXPRESS_BASE_URL || 'https://real-time-collaborative-notes-app-backend.onrender.com';
+export const EXPRESS_VERSION_URL = process.env.EXPRESS_VERSION_URL || 'https://real-time-collaborative-notes-app-backend.onrender.com/api/v1';
