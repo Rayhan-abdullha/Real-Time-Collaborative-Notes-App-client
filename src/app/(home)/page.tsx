375ad78c5d3514238d23add7d5b8c2898ffb0e00
Rayhan Abdullah <programmer.rayhan1@gmail.com>
2025-03-17 21:30:40 +0600

    fix: redirect issues

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,6 @@
 'use client'
-import useAuth from "@/hooks/useAuth";
 import NotesList from "../components/note/NoteList";
 function NotesPage() {
-    useAuth();
     return (
       <>        
         <NotesList/>
