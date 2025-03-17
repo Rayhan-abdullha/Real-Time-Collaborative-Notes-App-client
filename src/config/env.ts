94a1face8286091b0e3fb70ff715b9acfcca2bb8
Rayhan Abdullah <programmer.rayhan1@gmail.com>
2025-03-17 21:46:57 +0600

    fix: issues

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,4 +1,4 @@
-export const SITE_DOMAIN = 'http://localhost:3003/';
+export const SITE_DOMAIN = 'https://my-notes-io.vercel.app/';
 export const BACKEND_BASE_URL = 'https://real-time-note-app-40h4.onrender.com';
 export const EXPRESS_VERSION_URL = 'https://real-time-note-app-40h4.onrender.com/api/v1';
 // export const BACKEND_BASE_URL = 'http://localhost:4000';
